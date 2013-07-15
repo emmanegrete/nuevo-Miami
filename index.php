28a938678b0803f2539756c19c8a831194a461dd
emmanuel negrete <emmanuel@emmanuelnegrete.net>
2013-07-15 09:44:42 -0500

    cosas que ya hice 1

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Barezzito Miami</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="css/general.css">
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<div class="row-fluid">
+	<div class="span12">
+		<div class="centrado marco_Color texto_centrado">
+			<img class="logo_alto_ancho" src="img/logo-barezzito-miami-sushi-best-sushi-awesome-place-wild-party-drinks-miami-south-beach-florida-fun-crazy-good-food.jpg" alt="logo-barezzito-miami-sushi-best-sushi-awesome-place-wild-party-drinks-miami-south-beach-florida-fun-crazy-good-food">
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="al_Cien">
+				<div class="al_Setenta misBloques">
+					<div class="marco_Color">
+						<div class="misBloques btn_menu fuente_blanca">MENU</div>
+						<div class="misBloques btn_menu fuente_blanca">PROMOS</div>
+						<a href="#reservation"><div class="misBloques btn_menu fuente_blanca">RESERVATION</div></a>
+						<div class="misBloques btn_menu fuente_blanca">REVIEW</div>
+						<div class="misBloques btn_menu fuente_blanca">MEMBER</div>
+						<div class="misBloques btn_menu fuente_blanca">LOCATION</div>
+					</div>
+				</div>
+				<div class="al_Treinta marco_Color misBloques">
+					<div class="misBloques">
+						<a href="#email"><img class="icono_social" src="img/icono-email-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-email-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people"></a>
+					</div>
+					<div class="misBloques">
+						<a href="tel://1-305-397-8882"><img class="icono_social" src="img/icono-telephone-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-telephone-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-pretty-people"></a>
+					</div>
+					<div class="misBloques">
+						<a href="https://www.facebook.com/pages/Barezzito-South-Beach/178688168878223" target="_blank"><img class="icono_social" src="img/icono-face-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-face-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people"></a>
+					</div>
+					<div class="misBloques">
+						<a href="https://twitter.com/BarezzitoSB" target="_blank"><img class="icono_social" src="img/icono-twitter-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-twitter-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people"></a>
+					</div>
+					<div class="misBloques">
+						<a href="http://instagram.com/barezzitosouthbeach" target="_blank"><img class="icono_social" src="img/icono-instagram-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-instagram-fun-martini-miami-barezzito-bar-music-south-beach-sushi-collins-avenue-food-awesome-people"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="alCien marco_Color">
+		<div class="foodMenu misBloques marco_Color">
+			<div class="imagenMenu">
+				
+			</div>
+			<div class="centrado fuente_blanca texto_centrado">
+				MENU
+			</div>
+		</div>
+		<div class="cuadroOpcion misBloques marco_Color">
+			<div class="imagen_Sub"></div>
+			<div class="centrado fuente_blanca texto_centrado">
+				PROMOS
+			</div>
+		</div>
+		<div class="cuadroOpcion misBloques marco_Color">
+			<div class="imagen_Sub"></div>
+			<div class="centrado fuente_blanca texto_centrado">
+				RESERVATION
+			</div>
+		</div>
+		<div class="cuadroOpcion misBloques marco_Color">
+			<div class="imagen_Sub"></div>
+			<div class="centrado fuente_blanca texto_centrado">
+				WRITE A REVIEW
+			</div>
+		</div>
+		<div class="cuadroOpcion misBloques marco_Color">
+			<div class="imagen_Sub"></div>
+			<div class="centrado fuente_blanca texto_centrado">
+				BOOK EVENT
+			</div>
+		</div>
+		<div class="cuadroOpcion misBloques marco_Color texto_centrado">
+			<div class="cajaHorario fuente_blanca centrado fondo_rojo">
+				<div class="letra_chica_bold">MON-FRIDAY</div>
+				<div class="hour_open">4 PM - 5AM</div>
+				<div class="letra_chica_bold">SATURDAY</div>
+				<div class="hour_open">12PM - 5AM</div>
+				<div class="letra_chica_bold">SUNDAY</div>
+				<div class="hour_open">12PM - 2AM</div>
+			</div>
+		</div>
+		<div class="cuadroOpcion misBloques marco_Color">
+			<div class="imagen_Sub centrado texto_centrado">
+				<div class="fuente_blanca dosmil_grande">2000</div>
+				<div class="fuente_blanca collins_av">COLLINS AV.</div>
+				<div class="fuente_blanca miami_beach">MIAMI BEACH, FL</div>
+			</div>
+			<div class="centrado fuente_blanca texto_centrado">
+				305-397-8882
+			</div>
+		</div>
+	</div>
+
+	<!-- CONCEPT -->
+	<div class="al_Cien marco_Color fondo_blanco margin_arriba_grande padding_abajo_chico">
+		<div class="al_Cien misBloques">
+			<div class="secciones">
+				<div class="al_Noventa centrado">
+					<p>CONCEPT</p>
+					<p>12PM - 2AMThe pioneer bar and which has become a tradition in the city of Leon is BAREZZITO, born in November 2003. 
+
+Always Fun seven days a week, Barezzito Is the new spot with the Perfect. 
+
+SUSHI + BAR + MUSIC</p>
+				</div>
+			</div>
+			<div class="secciones">
+				<div class="al_Cien centrado">
+					
+				</div>
+			</div>
+		</div>
+		<div class="al_Cien misBloques marco_Color">
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
+		</div>
+	</div>
+	<div id="reservation" class="padding_abajo_mediano"></div>
+
+	<!-- RESERVATION -->	
+	<div class="al_Cien marco_Color  padding_abajo_chico">
+		<div class="fuente_blanca">
+			RESERVATION
+		</div>
+		<form action="">
+			
+			<div class="margin_lado_mediano al_Cien misBloques">
+				<label class="fuente_blanca" for=""> NAME </label>
+				<input type="text">
+			</div>
+			<div class="margin_lado_mediano al_Cien misBloques">
+				<label class="fuente_blanca" for="">E-MAIL</label>
+			<input type="text">
+			</div>
+			
+			<div class="al_Cuarenta misBloques margin_lado_mediano">
+				<label class="fuente_blanca" for="">DATE</label>
+				<input type="text">
+			</div> 
+			<div class="al_Cuarenta misBloques margin_lado_mediano">
+				<label class="fuente_blanca" for="">HOUR</label>
+				<input type="text">
+			</div>
+			<div class="al_Cuarenta misBloques margin_lado_mediano">
+				<label class="fuente_blanca" for="">PEOPLE</label>
+				<input type="text">
+			</div>
+			<div class="al_Cuarenta misBloques margin_lado_mediano">
+				<label class="fuente_blanca" for=""></label>
+				<input type="text">
+			</div>
+		</form>
+	</div>
+
+	<!-- BOOK EVENT -->
+	<div class="al_Cien marco_Color margin_arriba_grande padding_abajo_chico fondo_rojo">
+		<div class="secciones">
+			<div class="al_Noventa centrado fuente_blanca">
+				<p>
+					BOOK EVENT
+				</p>
+				<p>
+				Do you have a big  or private event coming up? 
+				This is your place.
+				Please contact SYAMA REYES for more info
+				</p>
+				<p>
+				SYAMA REYES
+				</p>
+				<p>
+					VIP COSTUMER RELATIONS
+				</p>
+				<p>
+					+1 954 536 5086
+				</p>
+			</div>
+		</div>
+		<div class="secciones">
+			<div class="al_Noventa centrado">
+				
+			</div>
+		</div>
+	</div>
+	
+</div>
+
+</body>
+<script src="js/jquery.js"></script>
+<script src="js/modernizr.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/general.js"></script>
+</html>
