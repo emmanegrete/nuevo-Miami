f8eb6f44b835485a3a96cbf157389abf6dc09fb9
emmanuel negrete <emmanuel@emmanuelnegrete.net>
2013-07-15 20:30:26 -0500

    avance 2
    se añadieron cosas y asi

--- index.php
+++ index.php
@@ -12,110 +12,143 @@
 <body>
 
 <div class="row-fluid">
-	<div class="span12">
-		<div class="centrado marco_Color texto_centrado">
-			<img class="logo_alto_ancho" src="img/logo-barezzito-miami-sushi-best-sushi-awesome-place-wild-party-drinks-miami-south-beach-florida-fun-crazy-good-food.jpg" alt="logo-barezzito-miami-sushi-best-sushi-awesome-place-wild-party-drinks-miami-south-beach-florida-fun-crazy-good-food">
-		</div>
-	</div>
-	<div class="row-fluid">
+	<div class="stickyMenu completo misBloques fondoNegro">
 		<div class="span12">
-			<div class="al_Cien">
-				<div class="al_Setenta misBloques">
-					<div class="marco_Color">
-						<div class="misBloques btn_menu fuente_blanca">MENU</div>
-						<div class="misBloques btn_menu fuente_blanca">PROMOS</div>
-						<a href="#reservation"><div class="misBloques btn_menu fuente_blanca">RESERVATION</div></a>
-						<div class="misBloques btn_menu fuente_blanca">REVIEW</div>
-						<div class="misBloques btn_menu fuente_blanca">MEMBER</div>
-						<div class="misBloques btn_menu fuente_blanca">LOCATION</div>
+			<div class="centrado marco_Color texto_centrado">
+				<a href="#home"><img class="logo_alto_ancho" src="img/logo-barezzito-miami-sushi-best-sushi-awesome-place-wild-party-drinks-miami-south-beach-florida-fun-crazy-good-food.jpg" alt="logo-barezzito-miami-sushi-best-sushi-awesome-place-wild-party-drinks-miami-south-beach-florida-fun-crazy-good-food"></a> 
+			</div>
+		</div>
+
+		<!-- MENU --> 
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="al_Cien">
+					<div class="al_Setenta misBloques">
+						<div class="marco_Color">
+							<a href="img/barezzito_south_beach_miami_florida.pdf" target="_blank"><div class="misBloques btn_menu fuente_blanca">MENU</div></a>
+							<a href="img/promos_barezzito_miami_food_drinks_happy_hour_sushi_champagne_martinis_fun_bar_collins_av_florida_southbeach_south_beach_great_sushi.jpg" target="_blank"><div class="misBloques btn_menu fuente_blanca">PROMOS</div></a> 
+							<a href="#reservation"><div class="misBloques btn_menu fuente_blanca">RESERVATION</div></a>
+							<a href="#writeReview"><div class="misBloques btn_menu fuente_blanca">REVIEW</div></a> 
+							<a href="#becomeAMember"><div class="misBloques btn_menu fuente_blanca">MEMBER</div></a> 
+							<a href="#location"><div class="misBloques btn_menu fuente_blanca">LOCATION</div></a> 
+						</div>
 					</div>
-				</div>
-				<div class="al_Treinta marco_Color misBloques">
-					<div class="misBloques">
-						<a href="#email"><img class="icono_social" src="img/icono-email-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-email-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people"></a>
-					</div>
-					<div class="misBloques">
-						<a href="tel://1-305-397-8882"><img class="icono_social" src="img/icono-telephone-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-telephone-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-pretty-people"></a>
-					</div>
-					<div class="misBloques">
-						<a href="https://www.facebook.com/pages/Barezzito-South-Beach/178688168878223" target="_blank"><img class="icono_social" src="img/icono-face-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-face-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people"></a>
-					</div>
-					<div class="misBloques">
-						<a href="https://twitter.com/BarezzitoSB" target="_blank"><img class="icono_social" src="img/icono-twitter-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-twitter-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people"></a>
-					</div>
-					<div class="misBloques">
-						<a href="http://instagram.com/barezzitosouthbeach" target="_blank"><img class="icono_social" src="img/icono-instagram-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-instagram-fun-martini-miami-barezzito-bar-music-south-beach-sushi-collins-avenue-food-awesome-people"></a>
+					<div class="al_Treinta marco_Color misBloques">
+						<div class="misBloques">
+							<a href="#contact"><img class="icono_social" src="img/icono-email-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-email-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people"></a>
+						</div>
+						<div class="misBloques">
+							<a href="tel://1-305-397-8882"><img class="icono_social" src="img/icono-telephone-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-telephone-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-pretty-people"></a>
+						</div>
+						<div class="misBloques">
+							<a href="https://www.facebook.com/pages/Barezzito-South-Beach/178688168878223" target="_blank"><img class="icono_social" src="img/icono-face-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-face-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people"></a>
+						</div>
+						<div class="misBloques">
+							<a href="https://twitter.com/BarezzitoSB" target="_blank"><img class="icono_social" src="img/icono-twitter-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-twitter-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people"></a>
+						</div>
+						<div class="misBloques">
+							<a href="http://instagram.com/barezzitosouthbeach" target="_blank"><img class="icono_social" src="img/icono-instagram-fun-martini-miami-barezzito-bar-music-south-beach-sushi-great-food-awesome-people.png" alt="icono-instagram-fun-martini-miami-barezzito-bar-music-south-beach-sushi-collins-avenue-food-awesome-people"></a>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</div><!-- MENU --> 
 	</div>
+
+	<div id="home" class="padding_subMenu"></div>
+
+	<!-- SUB MENU --> 
 	<div class="alCien marco_Color">
 		<div class="foodMenu misBloques marco_Color">
-			<div class="imagenMenu">
-				
-			</div>
-			<div class="centrado fuente_blanca texto_centrado">
-				MENU
-			</div>
+			<a href="img/barezzito_south_beach_miami_florida.pdf" target="_blank">
+				<div class="texto_centrado">
+					<img class="imagenMenu grayscale" src="img/menu_barezzitto_miami_sushi_asian_fusion_restaurant_south_beach_drinks_beers_champagne.jpg" alt="menu_barezzitto_miami_sushi_asian_fusion_restaurant_south_beach_drinks_beers_champagne">
+				</div>
+				<div class="centrado fuente_blanca texto_centrado letra_bold">
+					MENU
+				</div>
+			</a>
 		</div>
 		<div class="cuadroOpcion misBloques marco_Color">
-			<div class="imagen_Sub"></div>
-			<div class="centrado fuente_blanca texto_centrado">
-				PROMOS
-			</div>
+			<a href="img/promos_barezzito_miami_food_drinks_happy_hour_sushi_champagne_martinis_fun_bar_collins_av_florida_southbeach_south_beach_great_sushi.jpg" target="_blank">
+				<div class="texto_centrado">
+					<img class="grayscale" src="img/promos-food-happy-hour-drinks-barezzito-live-music-sushi-south-beach.jpg" alt="promos-food-happy-hour-drinks-barezzito-live-music-sushi-south-beach">
+				</div>
+				<div class="centrado fuente_blanca texto_centrado letra_bold">
+					PROMOS
+				</div>
+			</a>
 		</div>
-		<div class="cuadroOpcion misBloques marco_Color">
-			<div class="imagen_Sub"></div>
-			<div class="centrado fuente_blanca texto_centrado">
-				RESERVATION
+
+		<a href="#reservation">
+			<div class="cuadroOpcion misBloques marco_Color">
+				<div class="texto_centrado">
+					<img class="grayscale" src="img/barezzito-miami-event-fun-south-beach-collins-avenue-sushi-bar-music-live-music.jpg" height="800" width="800" alt="menu_barezzitto_miami_sushi_asian_fusion_restaurant_south_beach_drinks_beers_champagne">
+				</div>
+				<div class="centrado fuente_blanca texto_centrado letra_bold">
+					RESERVATION
+				</div>
 			</div>
-		</div>
-		<div class="cuadroOpcion misBloques marco_Color">
-			<div class="imagen_Sub"></div>
-			<div class="centrado fuente_blanca texto_centrado">
-				WRITE A REVIEW
+		</a>
+
+		<a href="#writeReview">
+			<div class="cuadroOpcion misBloques marco_Color">
+				<div class="texto_centrado">
+					<img class="grayscale" src="img/book-event-sushi-deliciuos-food-sushi-barezzito-open-late-7-days-a-week-fun-south-beach-florida.jpg" alt="book-event-sushi-deliciuos-food-sushi-barezzito-open-late-7-days-a-week-fun-south-beach-florida">
+				</div>
+				<div class="centrado fuente_blanca texto_centrado letra_bold">
+					WRITE A REVIEW
+				</div>
 			</div>
-		</div>
-		<div class="cuadroOpcion misBloques marco_Color">
-			<div class="imagen_Sub"></div>
-			<div class="centrado fuente_blanca texto_centrado">
-				BOOK EVENT
+		</a>
+
+		<a href="#bookEvent">
+			<div class="cuadroOpcion misBloques marco_Color">
+				<div class="texto_centrado">
+					<img class="grayscale" src="img/book-event-sushi-deliciuos-food-sushi-barezzito-open-late-7-days-a-week-fun-south-beach-florida-fun.jpg" alt="book-event-sushi-deliciuos-food-sushi-barezzito-open-late-7-days-a-week-fun-south-beach-florida">
+				</div>
+				<div class="centrado fuente_blanca texto_centrado letra_bold">
+					BOOK EVENT
+				</div>
 			</div>
-		</div>
+		</a>
 		<div class="cuadroOpcion misBloques marco_Color texto_centrado">
 			<div class="cajaHorario fuente_blanca centrado fondo_rojo">
-				<div class="letra_chica_bold">MON-FRIDAY</div>
+				<div class="letra_bold">MON-FRIDAY</div>
 				<div class="hour_open">4 PM - 5AM</div>
-				<div class="letra_chica_bold">SATURDAY</div>
+				<div class="letra_bold">SATURDAY</div>
 				<div class="hour_open">12PM - 5AM</div>
-				<div class="letra_chica_bold">SUNDAY</div>
+				<div class="letra_bold">SUNDAY</div>
 				<div class="hour_open">12PM - 2AM</div>
 			</div>
 		</div>
 		<div class="cuadroOpcion misBloques marco_Color">
-			<div class="imagen_Sub centrado texto_centrado">
-				<div class="fuente_blanca dosmil_grande">2000</div>
-				<div class="fuente_blanca collins_av">COLLINS AV.</div>
-				<div class="fuente_blanca miami_beach">MIAMI BEACH, FL</div>
-			</div>
-			<div class="centrado fuente_blanca texto_centrado">
-				305-397-8882
-			</div>
+			<a href="#location">
+				<div class="imagen_Sub centrado texto_centrado">
+					<div class="fuente_blanca dosmil_grande">2000</div>
+					<div class="fuente_blanca letra_mediana">COLLINS AV.</div>
+					<div class="fuente_blanca miami_beach">MIAMI BEACH, FL</div>
+				</div>
+				<div class="centrado fuente_blanca texto_centrado">
+					305-397-8882
+				</div>
+			</a>
 		</div>
-	</div>
+	</div><!-- SUB MENU --> 
 
 	<!-- CONCEPT -->
 	<div class="al_Cien marco_Color fondo_blanco margin_arriba_grande padding_abajo_chico">
 		<div class="al_Cien misBloques">
 			<div class="secciones">
 				<div class="al_Noventa centrado">
-					<p>CONCEPT</p>
+					<p class="letra_roja letra_mediana letra_bold">CONCEPT</p>
 					<p>12PM - 2AMThe pioneer bar and which has become a tradition in the city of Leon is BAREZZITO, born in November 2003. 
 
 Always Fun seven days a week, Barezzito Is the new spot with the Perfect. 
-
-SUSHI + BAR + MUSIC</p>
+					</p>
+					<p class="letra_roja letra_bold">
+						SUSHI + BAR + MUSIC
+					</p>
 				</div>
 			</div>
 			<div class="secciones">
@@ -138,15 +171,16 @@ SUSHI + BAR + MUSIC</p>
 			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
 			<div class="galeria_imagenes misBloques marco_Color">Uno</div>
 		</div>
-	</div>
-	<div id="reservation" class="padding_abajo_mediano"></div>
+	</div><!-- CONCEPT -->
+
+	<div id="reservation" class="padding_subMenu"></div>
 
 	<!-- RESERVATION -->	
 	<div class="al_Cien marco_Color  padding_abajo_chico">
-		<div class="fuente_blanca">
+		<div class="fuente_blanca letra_mediana letra_bold">
 			RESERVATION
 		</div>
-		<form action="">
+		<form>
 			
 			<div class="margin_lado_mediano al_Cien misBloques">
 				<label class="fuente_blanca" for=""> NAME </label>
@@ -173,23 +207,30 @@ SUSHI + BAR + MUSIC</p>
 				<label class="fuente_blanca" for=""></label>
 				<input type="text">
 			</div>
+			<div class="al_Treinta misBloques margin_lado_mediano texto_centrado">
+				<div id="envioReserva" class="fuente_blanca fondo_rojo padding_abajo_mediano letra_bold">
+					Reserve
+				</div>
+			</div>
 		</form>
-	</div>
+	</div><!-- RESERVATION -->
+	
+	<div id="bookEvent" class="padding_subMenu"></div>
 
 	<!-- BOOK EVENT -->
 	<div class="al_Cien marco_Color margin_arriba_grande padding_abajo_chico fondo_rojo">
-		<div class="secciones">
+		<div class="secciones misBloques">
 			<div class="al_Noventa centrado fuente_blanca">
-				<p>
+				<p class="letra_mediana letra_bold">
 					BOOK EVENT
 				</p>
 				<p>
-				Do you have a big  or private event coming up? 
-				This is your place.
-				Please contact SYAMA REYES for more info
+					Do you have a big  or private event coming up? 
+					This is your place.
+					Please contact SYAMA REYES for more info
 				</p>
-				<p>
-				SYAMA REYES
+				<p class="letra_bold">
+					SYAMA REYES
 				</p>
 				<p>
 					VIP COSTUMER RELATIONS
@@ -199,12 +240,154 @@ SUSHI + BAR + MUSIC</p>
 				</p>
 			</div>
 		</div>
-		<div class="secciones">
+		<div class="secciones misBloques">
 			<div class="al_Noventa centrado">
 				
 			</div>
 		</div>
-	</div>
+	</div><!-- BOOK EVENT -->
+	
+	<div id="writeReview" class="padding_subMenu"></div>
+
+	<!-- WRITE A REVIEW -->
+	<div class="al_Cien marco_Color misBloques padding_abajo_mediano">
+		<div class="secciones misBloques marco_Color">
+			<div class="al_Noventa misBloques">
+				<div class="fuente_blanca">
+					Ola k ase?
+				</div>
+			</div>
+		</div>
+		<div class="secciones misBloques marco_Color">
+			<div class="al_Noventa centrado fuente_blanca misBloques">
+				<p class="letra_mediana letra_bold">WRITE A REVIEW</p>
+				<p>If you have visited us, feel free to write a review on this sites</p>
+				<div class="para_review padding_abajo_mediano margin_arriba_mediano marco_Color">
+					<a href="http://www.yelp.com/biz/barezzito-south-beach-miami-beach" target="_blank">
+						<div class="al_Veinte marco_Color misBloques texto_centrado">
+							<img class="icono_social" src="img/icon_yelp_barezzito_miami_good_food_drinks_sushi_miamia_place_have_fun_cool_bar_live_music_305.png" alt="yelp_barezzito_miami_south_beach_sushi_bar_music_live_music_fun_martinis_champagne">
+						</div>
+						<div  class="al_Setenta marco_Color misBloques letra_roja letra_bold">
+							YELP
+						</div>
+					</a>
+				</div>
+				<div class="para_review padding_abajo_mediano margin_arriba_mediano marco_Color">
+					<a href="http://www.tripadvisor.com/Restaurant_Review-g34439-d4261108-Reviews-Barezzito_m-Miami_Beach_Florida.html" target="_blank">
+						<div class="al_Veinte marco_Color misBloques texto_centrado">
+							<img class="icono_social" src="img/icon_tripadvisors_miami_south_beach_awesome_food_sushi_miamia_beer_have_fun_cool_bar_live_music_305_music.png" alt="tripadvisors_barezzito_miami_sushi_bar_florida_great_place_hot_bar_martinis_drinks_beer_beautiful_people">
+						</div>
+						<div class="al_Setenta marco_Color misBloques letra_roja letra_bold">
+							TRIPADVISORS
+						</div>
+					</a>
+				</div>
+				<div class="para_review padding_abajo_mediano margin_arriba_mediano marco_Color">
+					<a href="http://www.urbanspoon.com/r/12/1752428/restaurant/Miami/Barezzito-Miami-Beach" target="_blank">
+						<div class="al_Veinte misBloques texto_centrado">
+							<img class="icono_social" src="img/icon_urbanspoon_barezzito_south_beach_good_food_drinks_sushi_miamia_place_have_fun_cool_bar_live_music_305.png" alt="urbanspoon_great_food_miami_florida_south_beach_drinks_awesome_place_sushi_asian_food_bar">
+						</div>
+						<div class="al_Setenta marco_Color misBloques letra_roja letra_bold">
+							URBANSPOON
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div><!-- WRITE A REVIEW -->
+	
+	<div id="becomeAMember" class="padding_subMenu"></div>
+
+	<!-- BECOME A MEMBER -->	
+	<div class="al_Cien marco_Color  padding_abajo_chico">
+		<div class="fuente_blanca letra_mediana letra_bold">
+			BECOME A MEMBER
+		</div>
+		<form>
+			
+			<div class="al_Cien misBloques">
+				<label class="fuente_blanca" for=""> NAME </label>
+				<input type="text">
+			</div>
+			<div class="al_Cien misBloques">
+				<label class="fuente_blanca" for="">E-MAIL</label>
+			<input type="text">
+			</div>
+			<div class="al_Cien misBloques">
+				<label class="fuente_blanca" for=""></label>
+				<input type="text">
+			</div>
+			<div class="al_Treinta misBloques margin_lado_mediano texto_centrado">
+				<div id="envioMiembro" class="fuente_blanca fondo_rojo padding_abajo_mediano letra_bold">
+					Send
+				</div>
+			</div>
+		</form>
+	</div><!-- BECOME A MEMBER -->
+	
+	<div id="location" class="padding_subMenu"></div>
+
+	<!-- LOCATION -->
+	<div class="al_Cien marco_Color margin_arriba_grande padding_abajo_chico fondo_rojo">
+		<div class="secciones misBloques">
+			<div class="al_Noventa centrado fuente_blanca color_azul">
+				<p class="letra_mediana letra_bold">
+					LOCATION
+				</p>
+				<p>
+					2000 Collins Avenue, Miami Beach <br>
+					FL 33139, USA<br>
+					CALL US  305-397-8882
+				</p>
+			</div>
+		</div>
+		<div class="secciones misBloques color_azul">
+			<div class="al_Noventa centrado fuente_blanca texto_centrado misBloques">
+				<p> <span class="letra_bold">MON-FRIDAY</span> 4 PM - 5AM</p>
+				<p> <span class="letra_bold">SATURDAY</span> 12PM - 5AM</p>
+				<p> <span class="letra_bold">SUNDAY</span> 12PM - 2AM</p>
+			</div>
+		</div>
+		<div class="completo misBloques">
+			<div class=" centrado">
+				<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.mx/maps/ms?msa=0&amp;msid=201804361596844124137.0004e190323cf8b76b864&amp;ie=UTF8&amp;ll=25.795718,-80.129256&amp;spn=0,0&amp;t=m&amp;output=embed"></iframe>
+			</div>
+		</div>
+	</div><!-- LOCATION -->
+	
+	<div id="contact" class="padding_subMenu"></div>
+
+	<!-- CONTACT -->	
+	<div class="al_Cien marco_Color  padding_abajo_chico">
+		<div class="fuente_blanca letra_mediana letra_bold">
+			CONTACT
+		</div>
+		<form>
+			
+			<div class="margin_lado_mediano al_Cien misBloques">
+				<label class="fuente_blanca" for=""> NAME </label>
+				<input type="text">
+			</div>
+			<div class="margin_lado_mediano al_Cien misBloques">
+				<label class="fuente_blanca" for="">E-MAIL</label>
+			<input type="text">
+			</div>
+			
+			<div class="al_Cuarenta misBloques margin_lado_mediano">
+				<label class="fuente_blanca" for="">MESSAGE</label>
+				<textarea name="" id=""></textarea>
+			</div> 
+			<div class="al_Cuarenta misBloques margin_lado_mediano">
+				<label class="fuente_blanca" for=""></label>
+				<input type="text">
+			</div>
+			<div class="al_Treinta misBloques margin_lado_mediano texto_centrado">
+				<div id="envioReserva" class="fuente_blanca fondo_rojo padding_abajo_mediano letra_bold">
+					Reserve
+				</div>
+			</div>
+		</form>
+	</div><!-- CONTACT -->
 	
 </div>
 
